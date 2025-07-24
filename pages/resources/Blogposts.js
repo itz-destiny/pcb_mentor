3f51eaca86aff15cc8c4cdf09017b84763cc1c4e
Owen-Jz <brumeegbagba101@gmail.com>
2025-07-24 14:21:43 +0200

    footer

--- pages/resources/Blogposts.js
+++ pages/resources/Blogposts.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import Image from "next/image";
 
 const BlogPage = () => {
